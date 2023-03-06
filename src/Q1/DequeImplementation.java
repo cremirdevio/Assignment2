1143b0448a32a02ee28d45450f8dc37f50811cec
Joseph <78092933+cremirdevio@users.noreply.github.com>
2023-03-06 12:47:02 -0800

    Update DequeImplementation.java

--- src/Q1/DequeImplementation.java
+++ src/Q1/DequeImplementation.java
@@ -1,20 +1,37 @@
 /**
- * 
- */
+
+This class provides an example usage of the Deque class to implement a deque with a string array of predetermined values.
+It adds all elements of the array to the deque, removes the last item and then prints all the remaining items in the deque in reverse order.
+@author user
+
+
+Test cases:
+When adding elements to the deque using addFirst() and addLast(), the size of the deque should increase by 1 each time.
+When removing elements from the deque using removeFirst() and removeLast(), the size of the deque should decrease by 1 each time.
+When peeking at the first or last element of the deque using peekFirst() or peekLast(), the element returned should match the expected value.
+When attempting to remove an element from an empty deque, a NoSuchElementException should be thrown.
+To run the test cases, create a new Deque object and call the necessary methods with expected input and assert the output using JUnit or any other testing framework.
+*/
 package Q1;
 
 import java.util.Iterator;
 
 /**
- * @author user
- *
+ * This class demonstrates how to use the Deque class to implement a stack.
  */
 public class DequeImplementation {
+	/**
+     * An array of strings to be added to the Deque.
+     */
 	static String[] dequeStr = {"eclipse", "implmentation", "search", "user design", "deque"};
 
-	/**
-	 * @param args
-	 */
+    /**
+     * The main method of the class. It creates a Deque, adds the elements of
+     * the dequeStr array to it, removes the last element, and prints the
+     * remaining elements in the deque in reverse order.
+     * 
+     * @param args the command line arguments (not used)
+     */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Deque<String> myQueue = new Deque<String>();
