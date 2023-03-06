0c613053ae23575dc9f79a7b764ccf22f66f6732
Joseph <78092933+cremirdevio@users.noreply.github.com>
2023-03-06 12:34:53 -0800

    Update Deque.java

--- src/Q1/Deque.java
+++ src/Q1/Deque.java
@@ -6,19 +6,33 @@ package Q1;
 import java.util.NoSuchElementException;
 
 /**
- * @author user
+ * Represents a double-ended queue (deque), which allows elements to be added and removed from both ends of the queue.
+ * The deque is implemented using a doubly-linked list data structure.
  *
+ * @param <T> the type of elements in the deque.
  */
 public class Deque<T> {
 	private int size;
     private Node<T> head;
     private Node<T> tail;
 
+    /**
+     * Represents a node in the doubly-linked list that implements the deque.
+     *
+     * @param <T> the type of the data stored in the node.
+     */
     private static class Node<T> {
         private T data;
         private Node<T> prev;
         private Node<T> next;
 
+	 /**
+         * Constructs a new node with the specified data and links to the previous and next nodes in the list.
+         *
+         * @param data the data stored in the node.
+         * @param prev the previous node in the list.
+         * @param next the next node in the list.
+         */
         public Node(T data, Node<T> prev, Node<T> next) {
             this.data = data;
             this.prev = prev;
@@ -26,20 +40,38 @@ public class Deque<T> {
         }
     }
 
+    /**
+     * Constructs a new empty deque.
+     */
     public Deque() {
         size = 0;
         head = null;
         tail = null;
     }
 
+    /**
+     * Checks whether the deque is empty.
+     *
+     * @return true if the deque is empty, false otherwise.
+     */
     public boolean isEmpty() {
         return size == 0;
     }
-
+ 
+    /**
+     * Returns the number of elements in the deque.
+     *
+     * @return the size of the deque.
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * Adds an element to the front of the deque.
+     *
+     * @param item the element to add.
+     */
     public void addFirst(T item) {
         Node<T> newNode = new Node<>(item, null, head);
         if (head != null) {
@@ -51,6 +83,11 @@ public class Deque<T> {
         size++;
     }
 
+    /**
+     * Adds an element to the end of the deque.
+     *
+     * @param item the element to add.
+     */
     public void addLast(T item) {
         Node<T> newNode = new Node<>(item, tail, null);
         if (tail != null) {
@@ -62,6 +99,13 @@ public class Deque<T> {
         size++;
     }
 
+	
+    /**
+     * Removes and returns the first element of the deque.
+     *
+     * @return the first element of the deque.
+     * @throws NoSuchElementException if the deque is empty.
+     */
     public T removeFirst() {
         if (isEmpty()) {
             throw new NoSuchElementException();
@@ -77,6 +121,12 @@ public class Deque<T> {
         return item;
     }
 
+    /**
+     * Removes and returns the last element of the deque.
+     *
+     * @return the last element of the deque.
+     * @throws NoSuchElementException if the deque is empty.
+     */
     public T removeLast() {
         if (isEmpty()) {
             throw new NoSuchElementException();
@@ -92,6 +142,12 @@ public class Deque<T> {
         return item;
     }
 
+    /**
+     * Returns the first element of the deque.
+     *
+     * @return the first element of the deque.
+     * @throws NoSuchElementException if the deque is empty.
+     */
     public T peekFirst() {
         if (isEmpty()) {
             throw new NoSuchElementException();
@@ -99,6 +155,12 @@ public class Deque<T> {
         return head.data;
     }
 
+     /**
+     * Returns the last element of the deque.
+     *
+     * @return the last element of the deque.
+     * @throws NoSuchElementException if the deque is empty.
+     */
     public T peekLast() {
         if (isEmpty()) {
             throw new NoSuchElementException();
